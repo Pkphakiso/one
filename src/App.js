356481354aa78394ce17e5df65310ad0fa28aed5
Pkphakiso <phakiso.ngwevu@gmail.com>
2022-05-31 15:28:18 +0200

    first commiting

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+       name
+      
+       <a>
+         <i class="fa-brands fa-twitter"></i>
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
